1c2d73e37d6e420912dfc57db139fa4cfd2378c0
Ignacio Carbajo <icarbajop@gmail.com>
2014-11-26 20:52:37 +0100

    Improve plot and series display

--- apis/github/search_repositories/search_repositories.py
+++ apis/github/search_repositories/search_repositories.py
@@ -18,24 +18,35 @@ def Main():
   languages = languages.dropna()
   languages.sort()
 
+  percentages = (languages.value_counts() / len(languages) * 100).map('{:,.2f} %'.format)
+
   print 'Languages percentage:'
-  print languages.value_counts() / len(languages) * 100
+  print percentages
 
   # Create plot
   x = [int(r.stargazers_count) for r in first_page]
   y = [int(r.forks) for r in first_page]
+
+  # Add one to every value for logarithmic scale
+  x = [val + 1 for val in x]
+  y = [val + 1 for val in y]
+
   area = [100 for r in first_page]
   names = [r.name for r in first_page]
   colors = np.random.rand(len(first_page))
   pl.scatter(x, y, s=area, c=colors, alpha=0.5)
   for i in range(0, len(x)):
-    pl.annotate(names[i], (x[i], y[i]))
-  pl.title("Scatter plot of forks and starrings")
+    pl.annotate(names[i], (x[i], y[i]), fontsize=2)
+  pl.title("All values are with addition of 1 (for the logarithmic scale)")
+  pl.xlabel("Stars")
+  pl.xscale("log")
+  pl.yscale("log")
+  pl.ylabel("Forks")
   pl.tight_layout()
   filepath = 'plots/apis/github/search_repositories.png'
   pl.savefig(filepath, figsize=(1020, 1020), dpi=300)
   pl.close()
-  print 'A chart was created at ' + filepath
+  print 'A chart with high resolution and small font size (to minimize overlaps) was created at ' + filepath
 
 if __name__ == "__main__":
   Main()
